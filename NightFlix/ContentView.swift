048c2b96258f6c2627a2798de8d18b356d53cec0
FrizzleM <tommaso.daddio@icloud.com>
2026-05-24 13:08:06 +0200

    Initial Commit

---
+++ NightFlix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NightFlix
+//
+//  Created by Tommaso d’Addio on 24/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NightFlix/NightFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightFlixApp.swift
+//  NightFlix
+//
+//  Created by Tommaso d’Addio on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct NightFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
